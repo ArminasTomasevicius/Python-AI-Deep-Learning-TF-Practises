44abb2d4947ffb266e5e30feb3eab1c921372479
ArminasTomasevicius <arminas.tomasevicius@gmail.com>
2017-02-19 16:30:27 +0200

    First commit
    Uploading small educational projects

---
+++ ChatBot_Movies/logic.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import data_utils
+import seq2seq_model
+
+def train():
+    #generate dataset
+    enc_train, dec_train = data_utils.prepare_custom_data(gConfig['working_directory'])
+
+    train_set - read
+
+def seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
+    return tf.nn.seq2seq.embedding_attention_seq2seq(
+        encoder_inputs, decoder_inputs, cell,
+        num_decoder_symbols=source_vocab_size,
+        num_decoder_symbols=target_vocab_size,
+        embedding_size=size,
+        output_projection=output_projection,
+        feed_previous=do_decode
+    )
---
+++ ChatBot_Movies/seq2seq_model.py
@@ -0,0 +1,6 @@
+#function for an input embedding
+
+def seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
+    return tf.nn.seq2seq.embedding_attention_seq2seq(
+
+    )
